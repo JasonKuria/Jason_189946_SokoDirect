a1c49f082593e4fd547a9bbf1ec7defcc08a48e6
JasonKuria <kuriajason32@gmail.com>
2026-07-01 08:10:57 +0300

    registering profile-related models in users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile, Speciality, Message
+
+# Register so they appear in the admin panel
+admin.site.register(Profile)
+admin.site.register(Speciality)
+admin.site.register(Message)
+
